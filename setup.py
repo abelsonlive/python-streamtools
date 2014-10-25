096a6520f6b9d83a23155dff9d54295f76eb10b4
abelsonlive <brianabelson@gmail.com>
2014-10-24 21:28:45 -0400

    vbump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # setup
 setup(
   name='python-streamtools',
-  version='0.0.9.2',
+  version='0.0.9.3',
   description='A python wrapper for streamtools: http://nytlabs.github.io/streamtools',
   long_description = "",
   classifiers=[
